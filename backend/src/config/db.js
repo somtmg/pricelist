e2430e53fa17c0c0ef626be3ad013a3e649a7893
somtmg <sombdrtmg1@gmail.com>
2025-09-12 19:44:16 +0545

    database

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const cred = process.env.DEV_DATABASE_URL;
+const cred = process.env.DATABASE_URL;
 const sequelize = new Sequelize(cred, {
   dialect: "postgres",
   logging: false,
